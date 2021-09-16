848b250efd8925b48c1d1d94a51492a4c160bb27
Trevor Tang <awpdata@gmail.com>
2021-09-16 15:18:49 -0700

    Error checking; fixed regex expressions

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from collections import deque
 
 while True:
     equation = input()
-    numbers = deque(re.findall(r"(-?[\d]+)+", equation))
-    operators = deque(re.findall(r"[()+*/-]+(?=\s)+?", equation))
+    numbers = deque(re.findall(r"(?<![\w])(-?[\d]+)(?![+\w-])", equation))  # Numbers must match regex
+    operators = deque(re.findall(r"(?<=\s)+[()+*/-]+(?=\s)+", equation))  # Operators must match regex
     if equation == "/exit":
         print("Bye!")
         break
@@ -13,13 +13,18 @@ while True:
     elif re.match(r"/[\w]+", equation) is not None:
         print("Unknown command")
     else:
-        if equation != "":
-            for operator in operators:
-                if re.match(r"[+]+", operator) is not None:
-                    numbers.appendleft(int(numbers.popleft()) + int(numbers.popleft()))
-                elif re.match(r"[-]+", operator) is not None:
-                    if len(operator) % 2 == 0:
+        if len(numbers) > 0:
+            try:
+                for operator in operators:
+                    if re.match(r"^[+]+$", operator) is not None:
                         numbers.appendleft(int(numbers.popleft()) + int(numbers.popleft()))
-                    else:
-                        numbers.appendleft(int(numbers.popleft()) - int(numbers.popleft()))
-            print(*numbers) if len(operators) != 0 or len(numbers) > 0 else print("Invalid expression")
+                    elif re.match(r"^[-]+$", operator) is not None:
+                        if len(operator) % 2 == 0:
+                            numbers.appendleft(int(numbers.popleft()) + int(numbers.popleft()))
+                        else:
+                            numbers.appendleft(int(numbers.popleft()) - int(numbers.popleft()))
+                print(*numbers) if len(numbers) == 1 else print("Invalid expression")
+            except IndexError:
+                print("Invalid expression")
+        else:
+            print("Invalid expression") if equation != "" else ""
