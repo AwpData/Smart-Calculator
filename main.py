2388bb56ae7bbace43ab1979e9a87037a0cbb1d1
Trevor Tang <awpdata@gmail.com>
2021-09-18 22:41:32 -0700

    Code cleanup; fixed postfix bug; other minor bug fixes

--- main.py
+++ main.py
@@ -34,80 +34,82 @@ while True:
             else:  # The right side is all numbers, so assign it this value
                 variables[variable_parts[0]] = variable_parts[1]
 
-
-
-
-    else:
-        # There are digits instead or the user wants to do math with variables
-        if re.search(r"(?<![\w])[-]?[\d]", equation) is not None or re.search("[A-Za-z]+", equation) is not None:
-            vars = equation.split()  # Split all numbers (or variables) by spaces first
+    else:  # This is the parser to convert the infix to postfix notation
+        if re.search(r"[\d]+[+/*-]-[\d]+", equation):  # Checks to make sure user did not put 5--5, 5+-5, etc.
+            print("Invalid expression 40")
+        elif re.match(r"^\+[\d]+$", equation):  # Checks if user did +#..., it will just print the number positive
+            print(equation[1:])
+        elif re.search(r"(?<![\w])[-]?[\d]", equation) is not None or re.search("[A-Za-z]+", equation) is not None:
+            vars = equation.split()
             operators = deque()  # This will store all of our operators for easy access (stack)
-            numbers = deque()  # This will store all of our numbers for easy access (queue)
+            computation = deque()  # This will store the postfix expression
             priorities = {"(": 0, "/": 2, "*": 2, "+": 1, "-": 1}
             for var in vars:
                 # If the number is a variable, add its value
                 if var.isalpha() and var in variables:  # Variable that is VALID
-                    numbers.append(variables[var])
+                    computation.append(variables[var])
                 elif var.isnumeric() or re.match(r"[+-][\d]+$", var) is not None:  # All digits
-                    numbers.append(var)
+                    computation.append(var)
                 elif re.search(r"[(]+[\d]+", var) is not None:  # Left parenthesis
-                    numbers.append("".join(re.findall("[\d]+", var)))
+                    computation.append("".join(re.findall("[\d]+", var)))
                     for _ in range(len(var) - 1):
                         operators.append("(")
+
                 elif re.search(r"[\d]+[)]+", var) is not None:  # Right parenthesis (pop until we find left p)
-                    numbers.append("".join(re.findall("[\d]+", var)))
+                    computation.append("".join(re.findall("[\d]+", var)))
                     operator = operators.pop()
                     while operator != "(":
-                        numbers.append(operator)
+                        computation.append(operator)
                         operator = operators.pop()
+
                 elif var.isalpha() and var not in variables:  # Invalid variable (STOP)
                     print("Unknown variable")
                     break
-                elif re.match("[*/+-]+$", var) is not None:  # Signage
+
+                elif re.match("[+-]+$", var) is not None or re.match("[*/]$", var) is not None:  # Signage
                     if len(var) % 2 == 0 and var.find("-") != -1:  # Two negatives = a positive
                         var = "+"
                     adj_operator = var[0]  # Get rid of all the duplicates
                     if len(operators) > 0:
                         operator = operators.pop()
-                        if priorities[adj_operator] < priorities[operator]:  # If the variable is less priority than op
-                            numbers.append(operator)  # Append greater one to numbers first
-                        else:
+                        while priorities[adj_operator] <= priorities[operator]:  # Keep popping if < due to PEMDAS
+                            computation.append(operator)  # Append greater one to numbers first
+                            if len(operators) > 0:
+                                operator = operators.pop()
+                            else:  # However, stop popping if the operator stack is empty
+                                break
+                        if priorities[adj_operator] > priorities[operator]:  # However, re-push the operator if >
                             operators.append(operator)
-                    operators.append(adj_operator)
+                    operators.append(adj_operator)  # All-in-all, push the new operator for further evaluation later
+
                 elif any(x.isalpha() for x in var) and any(x.isnumeric() for x in var):  # Makes sure var is valid
                     print("Invalid identifier")
                     break
-            while len(operators) > 0:
-                numbers.append(operators.pop())
+
+            while len(operators) > 0:  # There are no more symbols, push the rest of the operators 
+                computation.append(operators.pop())
+
             else:
-                try:  # if there was only a digit(s) (no operators), it will skip the loop and print the digit(s)
-                    num_stack = deque()
-                    while len(numbers) > 0:
-                        sign = numbers.popleft()
-                        if re.match(r"^[+]+$", sign) is not None:  # Check for addition sign(s)
+                try:
+                    num_stack = deque()  # Num_stack will only have #'s that lead to the result
+                    while len(computation) > 0:
+                        sign = computation.popleft()
+                        if re.match(r"^[+/*-]$", sign) is not None:
                             num2 = int(num_stack.pop())
                             num1 = int(num_stack.pop())
-                            num_stack.append(num1 + num2)
-                        elif re.match(r"^[-]+$", sign) is not None:  # Check for subtraction sign(s)
-                            num2 = int(num_stack.pop())
-                            num1 = int(num_stack.pop())
-                            if len(sign) % 2 == 0:  # Two negatives = a positive
+                            if sign == "+":
                                 num_stack.append(num1 + num2)
-                            else:
+                            elif sign == "-":
                                 num_stack.append(num1 - num2)
-                        elif re.match(r"^[*]+$", sign) is not None:
-                            num2 = int(num_stack.pop())
-                            num1 = int(num_stack.pop())
-                            num_stack.append(num1 * num2)
-                        elif re.match(r"^[/]+$", sign) is not None:
-                            num2 = int(num_stack.pop())
-                            num1 = int(num_stack.pop())
-                            num_stack.append(num1 // num2)
-                        else:
+                            elif sign == "*":
+                                num_stack.append(num1 * num2)
+                            elif sign == "/":
+                                num_stack.append(num1 // num2)
+                        else:  # The sign turned out to be a #
                             num_stack.append(sign)
                         # The expression is valid if it reduced it to one number
                     print(num_stack.pop()) if len(num_stack) == 1 else print("Invalid expression")
                 except IndexError and ValueError:
                     print("Invalid expression")
-        else:  # It is something I cannot comprehend
+        else:  # It is something the program cannot comprehend or empty input
             print("Invalid expression") if equation != "" else ""
